20efa3fb652511131be7f80c29fe904dc1363db5
ammu <ammuu1320@gmail.com>
2024-02-16 21:25:42 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://ammuu1320:ammuu1320@cluster0.dnwqqi3.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
